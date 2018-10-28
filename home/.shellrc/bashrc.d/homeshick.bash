ade36e126d9aa57e9258f45983ef844661b33f30
Hugues Chabot <hugues.chabot@gmail.com>
2018-10-27 22:41:56 -0400

    fixup! Add homeshick castle auto-refresh

--- home/.shellrc/bashrc.d/homeshick.bash
+++ home/.shellrc/bashrc.d/homeshick.bash
@@ -1,4 +1,4 @@
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
 
-printf '\nhomeshick --quiet refresh'
+homeshick --quiet refresh
